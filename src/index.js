863ed6602bbac74bd7a847d72c4464fa85154211
Hana Stupková <hstupkova@gmail.com>
2021-12-02 13:22:40 +0100

    using custom hook store

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import ProductsProvider from './context/products-context'
+import configureProductStore from './hooks-store/products-store';
+
+configureProductStore();
 
 ReactDOM.render(
-  <ProductsProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ProductsProvider>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+
+  document.getElementById('root'),
 );
